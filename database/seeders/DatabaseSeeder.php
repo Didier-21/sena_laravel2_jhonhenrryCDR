0ffc2ae721301fc70658992748c1f475fe6b5cbd
Didier Rojas <rojaspolaniadidierjhoan@gmail.com>
2024-09-28 11:57:28 -0500

    Se crea manejo de roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(RoleTableSeeder::class);
 
-        User::factory()->create([
+        $user = User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('password123'),
         ]);
+
+        $user->roles()->attach(Role::where('name', 'user')->first());
+
+        $user->save();
     }
 }
